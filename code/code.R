a5c9444947f7765ee8a8138a0ef84d840d8f2d17
svestis <pkoilakos@outlook.com>
2022-08-03 11:48:40 +0200

    Code

--- code/code.R
+++ code/code.R
@@ -1,3 +1,12 @@
 # Unit 1
+## 1.1
+### Read file
 crime<-read_sav("data/Crime.sav")
+
+## 1.2
+### Create a summary statistic of variable 'antisocx'
 summary(crime$antisocx)
+
+# Unit 2
+### Explore whether survey respondents experienced any crime in the 12 months prior to survey with the variable 'bcsvictim'
+table(crime$bcsvictim)
